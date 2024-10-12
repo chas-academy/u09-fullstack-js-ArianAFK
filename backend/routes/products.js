2eee61ec7cc4ccd3a0a40d87a2dac74a365f51d5
ArianAFK <arianfekri@hotmail.com>
2024-10-12 18:07:12 +0200

    post product model made and function updated

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Product = require('../models/productModel')
 
 const router = express.Router()
 
@@ -13,8 +14,15 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new product
-router.post('/', (req, res) => {
-    res.json({ msg: 'POST a new product' })
+router.post('/', async (req, res) => {
+    const { title, description, price, load } = req.body
+
+    try {
+        const product = await Product.create({ title, description, price, load })
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 })
 
 // DELETE product
