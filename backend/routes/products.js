628f42a688d2d0b4769a26ad3c7f8b54c062412f
ArianAFK <arianfekri@hotmail.com>
2024-10-12 19:30:40 +0200

    product controller with CRUD functions

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,38 +1,27 @@
 const express = require('express')
-const Product = require('../models/productModel')
+const {
+    getProducts,
+    getProduct,
+    createProduct,
+    deleteProduct,
+    updateProduct
+} = require('../controllers/productController')
 
 const router = express.Router()
 
 // GET all products
-router.get('/', (req, res) => {
-    res.json({ msg: 'GET all products' })
-})
+router.get('/', getProducts)
 
 // GET a single product
-router.get('/:id', (req, res) => {
-    res.json({ msg: 'GET a single product' })
-})
+router.get('/:id', getProduct)
 
 // POST a new product
-router.post('/', async (req, res) => {
-    const { title, description, price, load } = req.body
-
-    try {
-        const product = await Product.create({ title, description, price, load })
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
+router.post('/', createProduct)
 
 // DELETE product
-router.delete('/:id', (req, res) => {
-    res.json({ msg: 'DELETE a product' })
-})
+router.delete('/:id', deleteProduct)
 
 // UPDATE a product
-router.patch('/:id', (req, res) => {
-    res.json({ msg: 'UPDATE a product' })
-})
+router.patch('/:id', updateProduct)
 
 module.exports = router
