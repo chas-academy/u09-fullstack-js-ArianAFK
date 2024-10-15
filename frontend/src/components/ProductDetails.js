5366254dcdc9e3ae119a78f61fe3528bba2c4e0f
ArianAFK <arianfekri@hotmail.com>
2024-10-15 16:04:13 +0200

    delete products funktion

--- frontend/src/components/ProductDetails.js
+++ frontend/src/components/ProductDetails.js
@@ -1,9 +1,25 @@
+import { useProductsContext } from '../hooks/useProductsContext'
+
 const ProductDetails = ({ product }) => {
+    const { dispatch } = useProductsContext()
+
+    const handleClick = async () => {
+        const response = await fetch('/api/products/' + product._id, {
+            method: 'DELETE'
+        })
+        const json = await response.json()
+
+        if (response.ok) {
+            dispatch({ type: 'DELETE_PRODUCT', payload: json })
+        }
+    }
+
     return (
         <div className="product-details">
             <h4>{product.title}</h4>
             <p><strong>Includes: </strong>{product.description}</p>
             <p><strong>Price: </strong>{product.price} $/month</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     )
 }
