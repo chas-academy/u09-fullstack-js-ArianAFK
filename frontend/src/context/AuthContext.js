428df5b5ca48dd81b211c41c4bf9bf307c4cd1e7
ArianAFK <arianfekri@hotmail.com>
2024-10-17 17:47:24 +0200

    Auth context and hook

---
+++ frontend/src/context/AuthContext.js
@@ -0,0 +1,28 @@
+import { createContext, UseReducer } from 'react'
+
+export const AuthContext = createContext()
+
+export const authReducer = (statee, action) => {
+    switch (action.type) {
+        case 'LOGIN':
+            return { user: action.payload }
+        case 'LOGOUT':
+            return { user: null }
+        default:
+            return state
+    }
+}
+
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, {
+        user: null
+    })
+
+    console.log('AuthContext state: ', state)
+
+    return (
+        <AuthContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
